09167f13b060021b2c1b5b5156061a5a1827edc8
Tim Smith <tim@cozy.co>
2015-02-06 10:26:54 -0800

    Update gitignore and fix the license in the metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'npm_lazy'
 maintainer       'Tim Smith'
 maintainer_email 'tsmith84@gmail.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs and configures the npm_lazy application for caching NPM registry calls'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
