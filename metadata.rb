56b21a4dd6d2b065d6c1db4debb56ea562e03444
Tim Smith <tsmith84@gmail.com>
2015-08-31 23:06:05 -0700

    Add new metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.2'
 
 depends 'nodejs', '>= 2.0'
+
+source_url 'https://github.com/tas50/npm_lazy' if respond_to?(:source_url)
+issues_url 'https://github.com/tas50/npm_lazy/issues' if respond_to?(:issues_url)
