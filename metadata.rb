6f35b56d7c0d57ad4faff64635594b7d00610b0c
Tim Smith <tsmith84@gmail.com>
2019-10-13 20:17:47 -0700

    More cookstyle fixes for cookstyle 5.8
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license 'Apache-2.0'
 description 'Installs and configures the npm_lazy application for caching NPM registry calls'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
 
 supports 'ubuntu'
@@ -11,6 +10,6 @@ supports 'debian'
 
 depends 'nodejs', '>= 3.0'
 
-source_url 'https://github.com/sous-chefs/npm_lazy' if respond_to?(:source_url)
-issues_url 'https://github.com/sous-chefs/npm_lazy/issues' if respond_to?(:issues_url)
-chef_version '>= 12.11' if respond_to?(:chef_version)
+source_url 'https://github.com/sous-chefs/npm_lazy'
+issues_url 'https://github.com/sous-chefs/npm_lazy/issues'
+chef_version '>= 12.11'
