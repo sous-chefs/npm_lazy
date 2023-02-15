8fd26f83d5893550a59a50866404962d782d7bd7
Dan Webb <dan.webb@damacus.io>
2023-02-15 15:09:33 +0000

    Require Chef 16
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'help@sous-chefs.org'
 license 'Apache-2.0'
 description 'Installs and configures the npm_lazy application for caching NPM registry calls'
 version '2.0.6'
+source_url 'https://github.com/sous-chefs/npm_lazy'
+issues_url 'https://github.com/sous-chefs/npm_lazy/issues'
+chef_version '>= 16'
 
 supports 'ubuntu'
 supports 'debian'
 
 depends 'nodejs', '>= 3.0'
 
-source_url 'https://github.com/sous-chefs/npm_lazy'
-issues_url 'https://github.com/sous-chefs/npm_lazy/issues'
-chef_version '>= 12.11'
