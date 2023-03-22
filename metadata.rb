ae7b335043a599ea2d2820b01920f59fa994d654
Dan Webb <dan.webb@damacus.io>
2023-03-22 14:44:22 +0000

    chore: Update CI
    - Update overcommit
    - Update CI config
    - Update lint-unit workflow to 2.0.1

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,14 @@
-name 'npm_lazy'
-maintainer 'Sous Chefs'
+name             'npm_lazy'
+maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
-license 'Apache-2.0'
-description 'Installs and configures the npm_lazy application for caching NPM registry calls'
-version '3.0.0'
-source_url 'https://github.com/sous-chefs/npm_lazy'
-issues_url 'https://github.com/sous-chefs/npm_lazy/issues'
-chef_version '>= 16'
+license          'Apache-2.0'
+description      'Installs and configures the npm_lazy application for caching NPM registry calls'
+version          '3.0.0'
+source_url       'https://github.com/sous-chefs/npm_lazy'
+issues_url       'https://github.com/sous-chefs/npm_lazy/issues'
+chef_version     '>= 16'
 
 supports 'ubuntu'
 supports 'debian'
 
 depends 'nodejs', '>= 3.0'
-
